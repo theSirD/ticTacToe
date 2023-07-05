a3c788445f49bd3d5067b139fd505f32b14b9cdb
theSirD <isaev-daniel17@yandex.ru>
2023-07-05 12:34:35 +0500

    Make start button work

--- script.js
+++ script.js
@@ -3,55 +3,62 @@ const playerFactory = (mark) => {
     const getPoints = () => {
         return points;
     }
-    const playRound = (mark) => {
-        const gameBoardCells = document.querySelectorAll(".board-cell");
-        let cellWasChosen = false;
-        while (!false) {
-            gameBoardCells.forEach(cell => {
-                cell.addEventListener("click", (e) => {
-                    const firstIndex = cell.classList[1][0];
-                    const  secondIndex = cell.classList[1][1];
-                    cellWasChosen = true;
-                    return {firstIndex, secondIndex, mark};
-                })
+    const playRound = () => {
+        return new Promise((resolve) => { // Используем промис для ожидания выбора ячейки
+            const gameBoardCells = document.querySelectorAll(".board-cell");
+            let cellWasChosen = false;
+            
+            const cellClickHandler = (e) => {
+                const cell = e.target;
+                const firstIndex = cell.classList[1][5];
+                const secondIndex = cell.classList[1][6];
+                cellWasChosen = true;
+                resolve({ firstIndex, secondIndex, mark });
+            };
+
+            gameBoardCells.forEach((cell) => {
+                cell.addEventListener("click", cellClickHandler);
             });
-        }
-    }
+        });
+    };
     return {mark, getPoints, playRound};
 }
 
 const game = (() => {
     const gameBoard = (() => {
         const gameBoardCells = document.querySelectorAll(".board-cell");
-        let board =[[null, null, null],
-                [null, null, null],
-                [null, null, null]];
+        let board =[[2, 2, 2],
+                [2, 2, 2],
+                [2, 2, 2]];
 
-        const renderField = (board, gameBoardCells) => {
-            for (let i = 0; i < board.length; i++) {
+        const renderField = (board) => {
+            const gameBoardCells = document.querySelectorAll(".board-cell");
+            for (let i = 0; i < 3; i++) {
                 for (let j = 0; j < 3; j++) {
-                    if (board[i][j] === null) continue;
+                    if (board[i][j] === 2) continue;
                     else {
-                        if (board[i][j] === 1) gameBoardCells[i][j].insertAdjacentHTML("beforeend", "<p class='mark'>X</p>");
-                        else gameBoardCells[i][j].insertAdjacentHTML("beforeend", "<p class='mark'>O</p>");
+                        if (board[i][j] === 1) document.querySelector(`.data-${i}${j}`).insertAdjacentHTML("beforeend", "<p class='mark'>X</p>");
+                        else document.querySelector(`.data-${i}${j}`).insertAdjacentHTML("beforeend", "<p class='mark'>O</p>");
                     }
                 }
             }
         }
 
-        const clearField = (board) => {
-            for (let i = 0; i < board.length; i++) {
+        const clearField = () => {
+            console.log(board)
+            for (let i = 0; i < 3; i++) {
                 for (let j = 0; j < 3; j++) {
-                    board[i][j] = null;
-                   document.querySelector("mark").remove();
+                    board[i][j] = 2;
+                    if (document.querySelector(".mark"))  document.querySelector(".mark").remove();
+                    //
                 }
             }
         };
 
         const markCell = (cellToMark) => {
-            if (board[cellToMark.firstIndex][cellToMark.secondIndex] !== null) return;
+            if (board[cellToMark.firstIndex][cellToMark.secondIndex] !== 2) return;
             board[cellToMark.firstIndex][cellToMark.secondIndex] = cellToMark.mark === "X" ? 1 : 0;
-            renderField();
+            renderField(board);
         }
     
         return {clearField, markCell};
@@ -62,21 +69,23 @@ const game = (() => {
 
     const startGame = () => {
         const startButton = document.querySelector("button[type='button']");
-        startButton.addEventListener("click", (e) => {
+        startButton.addEventListener("click", async (e) => {
+            gameBoard.clearField();
             let i = 0;
             while ((playerO.getPoints() !== 3) && (playerX.getPoints() !== 3)) {
                 if (i % 2 === 0) {
-                    const cellToMark = playerX.playRound();
+                    const cellToMark = await playerX.playRound(); // Ожидание выбора ячейки
                     gameBoard.markCell(cellToMark);
                     ++i;
                 } else {
-                    const cellToMark = playerO.playRound();
+                    const cellToMark = await playerO.playRound(); // Ожидание выбора ячейки
                     gameBoard.markCell(cellToMark);
                     ++i;
                 }
             }
-        })
-    }
+        });
+    };
+    
     return {startGame};  //change that 
 })();
 
